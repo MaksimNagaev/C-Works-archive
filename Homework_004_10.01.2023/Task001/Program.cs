ac71f27ce92b8e12eadb377fe14acc14eb3feb22
maksimNagaev <Unforegiven@ya.ru>
2023-01-10 23:17:47 +0400

    Task001 complete

--- Homework_004_10.01.2023/Task001/Program.cs
+++ Homework_004_10.01.2023/Task001/Program.cs
@@ -4,5 +4,24 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+try
+{
 Console.WriteLine("Введите целое число");
 int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число - для возведения в степень первого числа");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(mathPowAlternate(a, b));
+}
+catch
+{
+    Console.WriteLine($"Вы ввели неверные данные");
+}
+int mathPowAlternate(int x, int y)
+{
+    int result = 1;
+    for (int i = 0; i < y; i++)
+    {
+        result = result * x;
+    }
+    return result;
+}
