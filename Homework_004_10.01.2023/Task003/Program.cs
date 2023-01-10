be97a5eb3d7b3e3782f936e90f649e6ac0d3bd2f
maksimNagaev <Unforegiven@ya.ru>
2023-01-11 00:04:45 +0400

    Task003 complete

---
+++ Homework_004_10.01.2023/Task003/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 целых чисел с клавиатуры 
+// и далее выводит массив на экран в одну строку.
+
+int[] arr = new int[8];
+fillarray(arr);
+printarray(arr);
+
+
+void fillarray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите значение для эллемента массива под индеком {i}");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void printarray(int[] newarray)
+{
+    foreach (var item in newarray)
+    {
+        Console.Write(item + " ");
+    }
+}
