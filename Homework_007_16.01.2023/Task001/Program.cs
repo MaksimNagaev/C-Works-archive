e0d28d373a637966c7f86b5ed632173e0a9c8488
maksimNagaev <Unforegiven@ya.ru>
2023-01-16 23:01:33 +0400

    Task001 complete

---
+++ Homework_007_16.01.2023/Task001/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+double[,] newarray = new double[rows, cols];
+FillArray(newarray);
+PrintArray(newarray);
+
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = Math.Round((new Random().NextDouble()*2-1)*100,3);
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],7} \t");
+        Console.WriteLine();
+    }
+}
