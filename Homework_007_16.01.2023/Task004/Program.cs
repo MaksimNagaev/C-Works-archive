3abde71045d689e368d3a85bc8ff4b34d0be01df
maksimNagaev <Unforegiven@ya.ru>
2023-01-17 00:23:36 +0400

    Task004 complete

---
+++ Homework_007_16.01.2023/Task004/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача HARD SORT необязательная. Считается за три обязательных
+// Задайте двумерный массив из целых чисел. Количество строк и столбцов задается с клавиатуры. Отсортировать элементы по возрастанию слева направо и сверху вниз.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 10 3
+// После сортировки
+// 1 2 3 4
+// 5 7 9 10
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] newarray = new int[rows, cols];
+FillArray(newarray);
+PrintArray(newarray);
+SortArray(newarray);
+Console.WriteLine("Сортированнный массив:");
+PrintArray(newarray);
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(-19, 20);
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3} \t");
+        Console.WriteLine();
+    }
+}
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int temp = 0;
+            for (int k = 0; k < array.GetLength(0); k++)
+            {
+                for (int l = 0; l < array.GetLength(1); l++)
+                {
+                    if (array[i, j] < array[k, l])
+                    {
+                        temp = array[i,j];
+                        array[i,j] = array[k,l];
+                        array[k,l] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
